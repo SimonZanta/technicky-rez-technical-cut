aa5af9a45842e075fc3ce9a4acb262ac80345998
Šimon Žanta <simon.zanta@protonmail.com>
2025-01-18 16:57:53 +0100

    feature/working slicing shader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EngineComponent } from "./core/engine/engine.component";
+import { SliceIncrementerComponent } from "./core/ui/slice-incrementer/slice-incrementer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EngineComponent],
+  imports: [RouterOutlet, EngineComponent, SliceIncrementerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/core/ui/slice-incrementer/slice-incrementer.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject, signal } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { GeometryService } from '../../engine/geometry/geometry.service';
+import * as THREE from 'three';
+
+@Component({
+  selector: 'app-slice-incrementer',
+  standalone: true,
+  imports: [MatSliderModule],
+  templateUrl: './slice-incrementer.component.html',
+  styleUrl: './slice-incrementer.component.scss'
+})
+export class SliceIncrementerComponent {
+
+  geometryService = inject(GeometryService);
+
+  slice = signal<number>(0.0);
+  lastPos = signal<number>(0.0);
+  
+
+  updateSlice(event: any) {
+    const oldPos = this.geometryService.slicers.at(0)?.position;
+    let newPos: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+    if(oldPos){
+      if(this.lastPos() < event.target.valueAsNumber){
+        newPos = new THREE.Vector3(0.0, 0.0, oldPos.z - Math.abs(event.target.valueAsNumber));
+        this.lastPos.set(event.target.valueAsNumber);
+      }else{
+        newPos = new THREE.Vector3(0.0, 0.0, oldPos.z + Math.abs(event.target.valueAsNumber));
+        this.lastPos.set(event.target.valueAsNumber);
+      }
+    }
+      this.geometryService.updateSlicerPosition(newPos);
+  }
+}
